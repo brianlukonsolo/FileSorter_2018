4b70ec168f4a54744d9bf925a9de1bde8e86b0e0
brianlukonsolo139 <brian.lukonsolo@gmail.com>
2018-01-27 18:08:27 +0000

    The application now generates a Jar file
    - added jar plugin to pom
    - moved configuration file
    - minor refactor

--- src/main/java/com/brianlukonsolo/App.java
+++ src/main/java/com/brianlukonsolo/App.java
@@ -10,6 +10,14 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class App {
     public static void main(String[] args) throws Exception {
+
+        if (args.length > 0){
+            for (int i=0;i<args.length;i++) {
+                System.out.println("\n\nARGUMENTS PROVIDED >>>> " + args[0] + "\n\n");
+                //TODO: implement logic such that the program can sort multiple folders
+            }
+        }
+
         SpringApplication.run(App.class);
 
         CamelContext context = new DefaultCamelContext();
